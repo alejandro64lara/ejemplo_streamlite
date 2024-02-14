21586bce4a36e2bb0f685f63c1d40e9e769df2d7
alejandro64lara <alejandro64lara@gmail.com>
2024-02-13 20:57:42 -0600

    app 2

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import streamlit as st
 
 st.header('Lanzar una moneda')
 
-st.slider(label, min_value=None, max_value=None, value=None, step=None, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible")
-
 number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
 start_button = st.button('Ejecutar')
 

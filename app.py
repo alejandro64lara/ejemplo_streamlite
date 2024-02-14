94ac2e2e3125abe3fc9acd1aa410ec16dd6777fe
alejandro64lara <alejandro64lara@gmail.com>
2024-02-13 20:40:10 -0600

    slider prueba 1

--- app.py
+++ app.py
@@ -2,4 +2,12 @@ import streamlit as st
 
 st.header('Lanzar una moneda')
 
-st.write('Esta aplicación aún no es funcional. En construcción.') 
+st.slider(label, min_value=None, max_value=None, value=None, step=None, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible")
+
+number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
+start_button = st.button('Ejecutar')
+
+if start_button:
+    st.write(f'Experimento con {number_of_trials} intentos en curso.')
+
+st.write('Esta aplicación aún no es funcional. En construcción.')
